c7e06188d6aa3a748057a072e523bfe4328fa016
mdekauwe <mdekauwe@gmail.com>
2019-02-12 21:30:23 +1100

    fixed comments

--- lotka_voltera.py
+++ lotka_voltera.py
@@ -13,17 +13,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import integrate
 
-
 def dX_dt(X, t, a, b, c, d):
     """ Return the growth rate of fox and rabbit populations. """
     return np.array([ a*X[0] - b*X[0]*X[1],
                      -c*X[1] + d*b*X[0]*X[1] ])
 
-a = 1.   # natural growing rate of rabbits, when there's no fox
-b = 0.1  # natural dying rate of rabbits, due to predation
-c = 1.5  # natural dying rate of fox, when there's no rabbit
-d = 0.75 # the factor describing how many caught rabbits let create a new fox
-t = np.linspace(0, 15, 1000)  # time
+a = 1.   # natural growth rate of rabbits, when there's no fox
+b = 0.1  # natural death rate of rabbits, due to predation
+c = 1.5  # natural death rate of fox, when there's no rabbit
+d = 0.75 # the factor describing how many caught rabbits create a new fox
+t = np.linspace(0, 15, 1000)     # time
 X0 = np.array([10, 5])           # initials conditions: 10 rabbits and 5 foxes
 X, infodict = integrate.odeint(dX_dt, X0, t, full_output=True, args=(a,b,c,d))
 
